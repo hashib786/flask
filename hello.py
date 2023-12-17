5b170cedfa6f02b7b57cbc890e375aca95469d7d
Hashib Raja <hashibraja23@gmail.com>
2023-12-17 19:42:25 +0530

    feat : added our first dynamic route

--- hello.py
+++ hello.py
@@ -9,6 +9,11 @@ def hello_world():
     return "<p>Hello, World! Hashib</p>"
 
 
+@app.route("/<name>")
+def say_hello(name):
+    return f"<h1>Hello, {name}</h1>"
+
+
 print(app)
 if (__name__ == "__main__"):
-    app.run()
+    app.run(debug=True)
